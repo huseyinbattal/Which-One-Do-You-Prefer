d73f9a641eb45fa763aee1752661715fb197dfbe
huseyinbatta <huseyinbattal42@gmail.com>
2022-05-07 23:04:46 +0300

    angular,react,vue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import angular from './angular.png';
+import react from './react.png';
+import vue from './vue.png';
+
+
 import './App.css';
 
+
+
 function App() {
+  const angularFunc = (e) => {
+    e.target.outerText = "-----ANGULAR.JS-----";
+  }
+  const reactFunc = (e) => {
+    e.target.outerText = "-----REACT.JS-----";
+  }
+  const vueFunc = (e) => {
+    e.target.outerText = "-----VUE.JS-----";
+  }
   return (
     <div className="App">
+          <h1>Which one do you prefer?</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={(e) => angularFunc(e)}><img src={angular} className="App-logo" alt="logo" /></button>
+        <button onClick={(e) => reactFunc(e)}><img src={react} className="App-logo" alt="logo" /></button>
+        <button onClick={(e) => vueFunc(e)}><img src={vue} className="App-logo" alt="logo" /></button>
       </header>
+ 
+
     </div>
   );
 }
